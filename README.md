6131aba6677503ddb880b1a37fe3fdeba5517208
circlemaker <45520511+cgefx@users.noreply.github.com>
2020-02-24 19:46:31 -0800

    add dynamic styles

--- README.md
+++ README.md
@@ -10,33 +10,33 @@ General instructions for all Pre-Work Projects can also be found in the Chingu V
 
 - [x] Page divided into calendar potion and event list portion.
 
-- [ ] **Calendar** portion to have:
+- [x] **Calendar** portion to have:
 
-  - [ ] Title and Current year in header.
-  - [ ] Current month between previous and upcoming month.
-  - [ ] Calendar dates of the month with display of previous month’s last dates if first day of current month does not start on a Sunday.
+  - [x] Title and Current year in header.
+  - [x] Current month between previous and upcoming month.
+  - [x] Calendar dates of the month with display of previous month’s last dates if first day of current month does not start on a Sunday.
   - [x] Arrows bordering the body of the calendar.
 
-- [ ] **Event** portion to have:
+- [x] **Event** portion to have:
 
-  - [ ]  Current day of the week displayed.
-  - [ ]  Current month and date (numerical) displayed.
-  - [ ]  List of Events with corresponding date of month divided by a vertical bar.
-  - [ ]  Add event button at bottom.
-  - [ ] *At least* **3** demo events in the event panel portion of the project.
-  - [ ] A modal form activated via button click that floats above the content.
+  - [x]  Current day of the week displayed.
+  - [x]  Current month and date (numerical) displayed.
+  - [x]  List of Events with corresponding date of month divided by a vertical bar.
+  - [x]  Add event button at bottom.
+  - [x] *At least* **3** demo events in the event panel portion of the project.
+  - [x] A modal form activated via button click that floats above the content.
 
 ### *Styling:*
 
-- [ ] Colors should match the theme outlined in the demo page
-- [ ] Previous month dates greyed out  `#696868`
-- [ ] Current month weekend dates and weekdays separate colors `#7cc0c9` `#839395`
-- [ ] Current date highlighted in a circular div `#26B9CC`
+- [x] Colors should match the theme outlined in the demo page
+- [x] Previous month dates greyed out  `#696868`
+- [x] Current month weekend dates and weekdays separate colors `#7cc0c9` `#839395`
+- [x] Current date highlighted in a circular div `#26B9CC`
 - [x] Gradient over background image (provided in assets folder)
-- [ ] Use corresponding fonts per section of the calendar page.
-- [ ] Invisible scroll bar for event section.
-- [ ] Event list should have at minimum three demo events hard coded in with the date on the *left of the horizontal bar* and the event name *on the right*
-- [ ] Utilize the following fonts in the calendar free for use from Google Fonts:
+- [x] Use corresponding fonts per section of the calendar page.
+- [x] Invisible scroll bar for event section.
+- [x] Event list should have at minimum three demo events hard coded in with the date on the *left of the horizontal bar* and the event name *on the right*
+- [x] Utilize the following fonts in the calendar free for use from Google Fonts:
   - *Montserrat*
   - *Open Sans*
   - *Open Sans Condensed*
@@ -52,16 +52,16 @@ General instructions for all Pre-Work Projects can also be found in the Chingu V
 
 #### *Upon Load:*
 
-- [ ] Calendar will display correct layout of the current month with the weeks starting on Sunday **No month scrolling is required**, only load the current month dates via *vanilla Javascript*
-- [ ] Will display last dates of previous month if the current month does not start on a Sunday.
-- [ ] A div will highlight the present day of the month as well as the current week date of the month (ex:// on Tuesday the 19th, both *Tues above the calendar dates and the 19th* will have special styling to indicate the present date!)
-- [ ] All other corresponding theme style will match the correct dates according to their place in the week
+- [x] Calendar will display correct layout of the current month with the weeks starting on Sunday **No month scrolling is required**, only load the current month dates via *vanilla Javascript*
+- [x] Will display last dates of previous month if the current month does not start on a Sunday.
+- [x] A div will highlight the present day of the month as well as the current week date of the month (ex:// on Tuesday the 19th, both *Tues above the calendar dates and the 19th* will have special styling to indicate the present date!)
+- [x] All other corresponding theme style will match the correct dates according to their place in the week
 
 #### *Modal Form:*
 
-- [ ] Be able to close form without submission and have form close on submission of event information
-- [ ] Have modal displayed as shown in the demo. This form is **not** expected to be functional.
-- [ ] Is hidden when not in use
+- [x] Be able to close form without submission and have form close on submission of event information
+- [x] Have modal displayed as shown in the demo. This form is **not** expected to be functional.
+- [x] Is hidden when not in use
 
 #### *Other:*
 
@@ -71,4 +71,4 @@ General instructions for all Pre-Work Projects can also be found in the Chingu V
 #### *Extras (Not Required)*
 
 - [ ] Make your design fully responsive *(small/large/portrait/landscape, etc.)*
-- [ ] optimized loading of calendar site (hint, consider the background image).
+- [x] optimized loading of calendar site (hint, consider the background image).
--- js/calendar.js
+++ js/calendar.js
@@ -1,23 +1,152 @@
-let today = new Date();
-
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-let todaysName = days[today.getDay()];
-let todaysNumber = today.getDate();
+const today = new Date();
+const todaysName = days[today.getDay()];
+const todaysNumber = today.getDate();
+
 let currentMonth = today.getMonth();
 let currentYear = today.getFullYear();
 let nextMonth = currentMonth + 1;
 let prevMonth = currentMonth - 1;
 
-//Get Calendar Headers
+//Get DOM Elements
 const yearHeader = document.getElementById("current-year");
 const currentDay = document.getElementById("current-day")
-let monthAndDate = document.getElementById("month-and-date")
+const monthAndDate = document.getElementById("month-and-date")
 const previousArrow = document.querySelector(".go-to-prev");
 const nextArrow = document.querySelector(".go-to-next");
-const tbl = document.getElementById("calendar-body");
+const calendarWeekHeader = document.querySelector(".calendar__week-header")
+const calendarTable = document.getElementById("calendar-body");
+
+function showCalendar(month, year) {
+  yearHeader.innerHTML = year;
+  calendarWeekHeader.innerHTML = "";
+  calendarTable.innerHTML = "";
+
+  //Creating the Days of Week Row
+  let headerRow = document.createElement('tr');
+  for (let i = 0; i < 7; i++) {
+    let headerCell = document.createElement("th");
+    let shortDay = days[i].slice(0, 3);
+    let headerCellText = document.createTextNode(shortDay);
+    if (i === 0 || i === 6) {
+      headerCell.classList.add("calendar_week-header--weekend")
+    }
+    if (days[i] === todaysName) {
+      headerCell.classList.add("current-day-of-week")
+    }
+    headerCell.appendChild(headerCellText);
+    headerRow.appendChild(headerCell);
+  }
+  calendarWeekHeader.appendChild(headerRow);
+
+  //Getting Numerical Dates to render
+  const daysPrevMonth = getLeadingDays(currentMonth, currentYear);
+  const daysThisMonth = getMonthDays(currentMonth, currentYear);
+  const daysNextMonth = getTrailingDays(daysPrevMonth, daysThisMonth);
+
+  let daysToRender = [...daysPrevMonth, ...daysThisMonth, ...daysNextMonth];
+
+  //Building the Calendar Table Body 
+  let dayToRender = 0;
+  let isMonthCurrent = false;
+  for (let i = 0; i < 6; i++) {
+    let row = document.createElement("tr");
+    for (let j = 0; j < 7; j++) {
+      if (daysToRender[dayToRender] === 1) {
+        isMonthCurrent = !isMonthCurrent;
+      }
+      let cell = document.createElement("td");
+      let cellText = document.createTextNode(daysToRender[dayToRender])
+      if (!isMonthCurrent) {
+        cell.classList.add("not-current-month");
+      }
+
+      if (
+        daysToRender[dayToRender] === today.getDate() &&
+        currentYear === today.getFullYear() &&
+        currentMonth === today.getMonth()
+      ) {
+        cell.classList.add("current-day");
+      }
+      if (j === 0 || j === 6) {
+        cell.classList.add("calendar_week-days--weekend")
+      }
+      cell.appendChild(cellText)
+      row.appendChild(cell)
+      dayToRender++;
+    }
+    calendarTable.appendChild(row);
+  }
+
+}
+
+//Helper Functions for Rendering Day logic
+function getLeadingDays(currentMonth, currentYear) {
+  let staDay = 0// 0, sunday, 1: monday
+  const ret = [];
+  const year = currentYear
+  const month = currentMonth
+  const firstWeekday = new Date(year, month, 1).getDay();
+  const days = firstWeekday + 7 - (staDay + 7) - 1; // 2 days become 1 for [1, 0]
+  for (let i = days * -1; i <= 0; i++) {
+    ret.push(new Date(year, month, i).getDate());
+  }
+  return ret;
+}
+
+function getMonthDays(currentMonth, currentYear) {
+  const ret = [];
+  const year = currentYear
+  const month = currentMonth
+  const lastDay = new Date(year, month + 1, 0).getDate();
+  for (let i = 1; i <= lastDay; i++) ret.push(i);
+  return ret;
+}
+
+function getTrailingDays(leadingDays, monthDays) {
+  const ret = [];
+  const days = 42 - (leadingDays.length + monthDays.length);
+  for (let i = 1; i <= days; i++) ret.push(i);
+  return ret;
+}
+
+function showMonths(currentMonth) {
+  let monthsHeader = document.getElementById("months-header");
+
+  monthsHeader.innerHTML = "";
+
+  let monthsToRender = [];
+
+  let prevMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+  let nextMonth = (currentMonth === 11) ? 0 : currentMonth + 1;
+
+  monthsToRender.unshift(months[prevMonth], months[currentMonth], months[nextMonth]);
+
+  monthsToRender.forEach(month => {
+    let headerMonth = document.createElement("p");
+    let monthText = document.createTextNode(month);
+
+    if (month === months[currentMonth]) {
+      headerMonth.classList.add("current-month")
+    }
+    headerMonth.appendChild(monthText);
+    monthsHeader.appendChild(headerMonth);
+  });
+
+  return monthsToRender;
+}
+
+function showEvents() {
+  let currentMonth = today.getMonth();
+  monthAndDate.innerHTML = `${months[currentMonth]} ${todaysNumber}`;
+  currentDay.innerHTML = todaysName;
+}
+
+function daysInMonth(iMonth, iYear) {
+  return 32 - new Date(iYear, iMonth, 32).getDate();
+}
 
 //Event Listeners 
 previousArrow.addEventListener("click", previous)
@@ -35,81 +164,6 @@ function next() {
   currentMonth = (currentMonth + 1) % 12;
   showCalendar(currentMonth, currentYear);
   showMonths(currentMonth);
-}
-
-function showMonths(currentMonth) {
-  let monthsHeader = document.getElementById("months-header");
-
-  monthsHeader.innerHTML = "";
-
-  let monthHeader = [];
-
-  let prevMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-  let nextMonth = (currentMonth === 11) ? 0 : currentMonth + 1;
-
-  monthHeader.unshift(months[prevMonth], months[currentMonth], months[nextMonth]);
-
-  monthHeader.forEach(month => {
-    let headerMonth = document.createElement("p");
-    let monthText = document.createTextNode(month);
-
-    if (month === months[currentMonth]) {
-      headerMonth.classList.add("current-month")
-    }
-    headerMonth.appendChild(monthText);
-    monthsHeader.appendChild(headerMonth);
-  });
-
-  return monthHeader;
-}
-
-function showEvents() {
-  let currentMonth = today.getMonth();
-  monthAndDate.innerHTML = `${months[currentMonth]} ${todaysNumber}`;
-  currentDay.innerHTML = todaysName;
-}
-
-function showCalendar(month, year) {
-  let firstDay = (new Date(year, month)).getDay();
-  tbl.innerHTML = "";
-  yearHeader.innerHTML = year;
-
-  //Making the table
-  let date = 1;
-  for (let i = 0; i < 6; i++) {
-    let row = document.createElement("tr");
-
-    //creating individual cells, filing them up with data.
-    for (let j = 0; j < 7; j++) {
-      if (i === 0 && j < firstDay) {
-        cell = document.createElement("td");
-        cellText = document.createTextNode("");
-        cell.appendChild(cellText);
-        row.appendChild(cell);
-      }
-      else if (date > daysInMonth(month, year)) {
-        break;
-      }
-      else {
-        cell = document.createElement("td");
-        cellText = document.createTextNode(date);
-        if (
-          date === today.getDate() &&
-          year === today.getFullYear() &&
-          month === today.getMonth()) {
-          cell.classList.add("current-day");
-        }
-        cell.appendChild(cellText);
-        row.appendChild(cell);
-        date++;
-      }
-    }
-    tbl.appendChild(row);
-  }
-}
-
-function daysInMonth(iMonth, iYear) {
-  return 32 - new Date(iYear, iMonth, 32).getDate();
 }
 
 showMonths(currentMonth);
